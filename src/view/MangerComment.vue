803a1037325ca7c049b604a112af84617af5db0c
she_say <1132256103@qq.com>
2019-06-11 20:38:24 +0800

    评论管理

--- src/view/MangerComment.vue
+++ src/view/MangerComment.vue
@@ -1,75 +1,127 @@
 <template>
   <div>
-    <div class="commentItem" v-for="item in commitList" :key="item.commentId">
-      <Row type="flex" justify="space-between">
-        <Col>
-          <Avatar style="color: #f56a00;background-color: #fde3cf">U</Avatar>
-          <span>{{item.user}}</span>
-        </Col>
-        <Col>
-          <div class="itme">
-            <Time :time="item.time"/>
-          </div>
-        </Col>
-      </Row>
-      <Row style="padding:15px 20px">
-        <p>{{item.content}}</p>
-      </Row>
-      <span class="replay" @click="replay(item)">回复</span>
-      <span
-        class="delete"
-        @click="deleteIt(item)"
-        v-if="$store.state.userInfo.userName == item.user"
-      >删除</span>
-    </div>
+    <Table class="table" border :columns="columns" :data="List">
+      <template slot-scope="{ row }" slot="time">
+        <Time :time="row.time"></Time>
+      </template>
+    </Table>
+    <Page class="page" :current="current" :page-size="page_size" :total="total" @on-change="changPage"/>
   </div>
 </template>
 <script>
-
 import CONST from "../common/index.js";
 
 export default {
   data() {
     return {
       List: [],
+      current:1,
+      page_size: 5,
+      total: 20,
+      columns: [
+        {
+          title: "id",
+          key: "commentId"
+        },
+        {
+          title: "用户名",
+          key: "user"
+        },
+        {
+          title: "评论内容",
+          key: "content"
+        },
+        {
+          title: "时间",
+          key: "time",
+          slot: "time"
+        },
+        {
+          title: "操作",
+          key: "action",
+          width: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params);
+                    }
+                  }
+                },
+                "删除"
+              )
+            ]);
+          }
+        }
+      ]
     };
   },
-  components: {
-      
+  components: {},
+  methods: {
+    remove(i) {
+      console.log(i);
+      this.axios({
+        url: "http://192.168.43.138:9000/comment/" + i.row.commentId,
+        method: "delete"
+      })
+        .then(res => {
+          console.log(res);
+          if (res.data == "success") {
+            this.$Message.success("删除成功");
+            this.getComment(this.current)
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    changPage(e) {
+      this.current=e
+      this.getComment(e);
+    },
+    getComment(page) {
+      this.axios({
+        method: "put",
+        url: "http://192.168.43.138:9000/comment/" + page
+      })
+        .then(res => {
+          console.log(res.data);
+          this.List = res.data.rows;
+          this.total = res.data.totals;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   },
   created() {
-    this.axios({
-      method: "get",
-      url: "http://192.168.43.138:9000/task/-1"
-    })
-      .then(res => {
-        this.List = res.data.filter((item, index, array) => {
-          return item.type == "trade";
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.getComment(1);
   }
 };
 </script>
 <style scoped>
-.row {
-  border: 1px red solid;
-}
-.col {
-  /* width: 100%; */
-  height: 300px;
-}
-.task {
-  width: 100%;
-  height: 300px;
-}
 .no {
   width: 20%;
   margin: 100px 50%;
   transform: translateX(-50%);
   text-align: center;
+}
+.table {
+  width: 100%;
+  overflow: hidden;
+}
+.page {
+  width: 30%;
+  margin: 10px 50%;
+  transform: translateX(-50%);
 }
 </style>
 
